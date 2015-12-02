e19e14f8e2d95085bf838b8d589c6fbf71ce0f89
Alan Ott <alan@softiron.co.uk>
2015-12-02 02:35:11 +0100

    rpm/config.sh: Add default configuration, turn off VANILLA_ONLY

--- rpm/config.sh
+++ rpm/config.sh
@@ -1,9 +1,9 @@
 # The version of the main tarball to use
 SRCVERSION=4.4-rc2
 # variant of the kernel-source package, either empty or "-rt"
-VARIANT=
+VARIANT=-default
 # Set to 1 if the tree does not contain any non-vanilla patches at all
-VANILLA_ONLY=1
+VANILLA_ONLY=0
 # buildservice projects to build the kernel against
 OBS_PROJECT=openSUSE:13.2:Update
 IBS_PROJECT=
